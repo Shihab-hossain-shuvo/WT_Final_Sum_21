ccf8fa701cacc78de2514676e85459893f3e91a7
shihab <shuvokhanrs2015@gmail.com>
2021-08-17 22:34:07 +0600

    project admin

--- projectadmin/Views/Footer.php
+++
@@ -1,6 +0,0 @@
-<html>
-    <head></head>
-	<footer>
-	    <h4 align="center">Final_labtask_2</h4>
-	</footer>
-</html>
--- projectadmin/Views/MainHeader.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head></head>
-	<body>
-	    <div align = "center">
-		    <h3>Final_labtask_2</h3>
-		</div>
-	</body>
-</html>
